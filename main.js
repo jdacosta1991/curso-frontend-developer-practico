3ea0a549ca11bd05bd52d90db13834627240b32b
Jorge Da Costa <workdacojo@gmail.com>
2022-11-18 09:12:05 -0400

    maquetacion de product list

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const mobileMenu = document.querySelector(".mobile-menu");
 //
 const menuCarroIcon = document.querySelector(".navbar-shopping-cart");
 const menuCarroOrders = document.querySelector(".product-detail");
+const cardContainer = document.querySelector(".cards-container");
+
 
 /*Agregamos el addEvenListener para escuchar el evento del menu 
 inactivo*/
@@ -35,7 +37,8 @@ function toggleDesktopMenu() {
 
 //Creamos la funcion del menu hamburguesa
 function toggleMobileMenu() {
-  const isMenuCarroOrdersClosed = menuCarroOrders.classList.contains("inactive");
+  const isMenuCarroOrdersClosed =
+    menuCarroOrders.classList.contains("inactive");
   if (!isMenuCarroOrdersClosed) {
     menuCarroOrders.classList.add("inactive");
   }
@@ -59,3 +62,67 @@ function toggleCarritoMenu() {
 
   menuCarroOrders.classList.toggle("inactive");
 }
+
+/* Traer shoplist consulta */
+
+const productList = [];
+productList.push({
+  name: "bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Pantalla",
+  price: 220,
+  image: "/home/jorge/Descargas/pexels-cristian-benavides-2049411.jpg",
+});
+productList.push({
+  name: "Laptop",
+  price: 330,
+  image: "/home/jorge/Descargas/pexels-life-of-pix-7974.jpg",
+});
+
+//funcion para iterar y maquetar
+function renderProducts(arr){
+  //Iteramos por cada producto que tengamos
+for (product of arr) {
+  //Por Cada uno maquetamos o creamos en HTML
+  const productCard = document.createElement("div")
+  productCard.classList.add("product-card");
+
+  const imgCard = document.createElement("img")
+  imgCard.setAttribute("src", product.image)
+
+  const productInfo = document.createElement("div")
+  productInfo.classList.add("product-info");
+
+  const productInfoDiv = document.createElement("div")
+
+  const productPrice = document.createElement("p")
+  productPrice.innerText = "$" + product.price
+  const productName = document.createElement("p")
+  productName.innerText = product.name
+
+  productInfoDiv.appendChild(productPrice)
+  productInfoDiv.appendChild(productName)
+  
+  const productInfoFigure = document.createElement("figure")
+  const productImgCart = document.createElement("img")
+  productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg")
+
+  //los agregamos dinamicamente con javascript
+  productInfoFigure.appendChild(productImgCart);
+  
+  productInfo.appendChild(productInfoDiv)
+  productInfo.appendChild(productInfoFigure)
+
+  productCard.appendChild(imgCard)
+  productCard.appendChild(productInfo)
+
+  cardContainer.appendChild(productCard)
+}
+}
+
+
+renderProducts(productList);
